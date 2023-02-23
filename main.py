3c3d9956043e5aa9c1e4897f04d30a54756b926e
Firdavs0636 <65146478+Firdavs0636@users.noreply.github.com>
2023-02-23 22:11:27 +0900

    new

---
+++ main.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from math import floor
+import pygame.mixer
+
+# ---------------------------- CONSTANTS ------------------------------- #
+
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 15
+SECOND = 1000
+reps = 0
+ticks = ''
+timer = None
+
+# ---------------------------- PLAY SOUND ------------------------------- #
+pygame.mixer.init()
+
+
+def play(key):
+    if key == 'short':
+        pygame.mixer.music.load('mixkit-bike-magical-bell-591.wav')
+        pygame.mixer.music.play(loops=0)
+    elif key == 'long':
+        pygame.mixer.music.load('mixkit-bell-sound-with-delay-585.wav')
+        pygame.mixer.music.play(loops=0)
+    elif key == 'work':
+        pygame.mixer.music.load('tick-tock-timer-1046.wav')
+        pygame.mixer.music.play(loops=32)
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset():
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text='00:00')
+    title_label.config(text='Timer', fg=GREEN)
+    tick_label.config(text=ticks)
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        countdown(long_break)
+        title_label.config(text='BREAK', fg=RED)
+        play('long')
+    elif reps % 2 == 0:
+        countdown(short_break)
+        title_label.config(text='BREAK', fg=PINK)
+        play('short')
+    else:
+        countdown(work_sec)
+        title_label.config(text='WORK', fg=GREEN)
+        play('work')
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(remainder):
+    global ticks
+    global timer
+    count_min = floor(remainder / 60)
+    count_sec = remainder % 60
+    if count_sec < 10:
+        count_sec = f'0{count_sec}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if remainder >= 0:
+        timer = window.after(SECOND, countdown, remainder - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            ticks += '️✔︎'
+            tick_label.config(text=ticks)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Labels
+title_label = Label(text='Timer', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+
+tick_label = Label(bg=YELLOW, fg=GREEN, font=22)
+tick_label.grid(column=1, row=3)
+
+# Buttons
+start_bt = Button(text='Start', highlightthickness=0, bg=YELLOW, command=start_timer)
+start_bt.grid(column=0, row=2)
+
+reset_bt = Button(text='Reset', highlightthickness=0, bg='white', command=reset)
+reset_bt.grid(column=2, row=2)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
+
+window.mainloop()
